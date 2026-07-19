4fc326dbacb7c884768b177130cd72747bef6b43
TuranU77 <design@usuk.de>
2026-07-19 17:18:17 +0200

    Add Sites worker deployment wrapper

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <iframe
+      allow="pointer-lock; fullscreen"
+      className="site-frame"
+      src="/static/index.html"
+      title="Code Solo"
+    />
+  );
+}
